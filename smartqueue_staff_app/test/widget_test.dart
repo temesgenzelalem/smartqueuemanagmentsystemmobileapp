c2d4f4440cd07a6978001f435dce9552cd7788d4
Temesgen Zelalem <temesgenzelalem167@gmail.com>
2026-05-28 15:48:33 +0300

    fix widget test for unified login screen

--- smartqueue_staff_app/test/widget_test.dart
+++ smartqueue_staff_app/test/widget_test.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smartqueue_staff_app/main.dart';
 
 void main() {
   testWidgets('shows login screen', (tester) async {
+    SharedPreferences.setMockInitialValues({});
     await tester.pumpWidget(const SmartQueueStaffApp());
-    expect(find.text('Staff Login'), findsOneWidget);
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
+    expect(find.text('Sign in as staff or customer'), findsOneWidget);
   });
 }
